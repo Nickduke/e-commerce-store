858cba6f27bf1b5d13a9a0da8662e57e1bedd97e
Nick <ntoukas.nick@gmail.com>
2022-12-23 14:22:19 -0500

    feat: complete single product functionality

--- src/pages/product.js
+++ src/pages/product.js
@@ -25,6 +25,26 @@ window.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(`${singleProductUrl}${urlID}`);
     if (response.status >= 200 && response.status <= 299) {
       const product = await response.json();
+
+      const { id, fields } = product;
+      productID = id;
+
+      const { name, company, price, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+
+      document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home / ${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = `by ${company}`;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colors.forEach((color) => {
+        const span = document.createElement('span');
+        span.classList.add('product-color');
+        span.style.backgroundColor = `${color}`;
+        colorsDOM.appendChild(span);
+      });
     } else {
       console.log(response.status, response.statusText);
       centerDOM.innerHTML = `
@@ -40,4 +60,8 @@ window.addEventListener('DOMContentLoaded', async () => {
   }
 
   loading.style.display = 'none';
+});
+
+cartBtn.addEventListener('click', () => {
+  addToCart(productID);
 });
