bb1375167b129ff7dc3da37b4b1bd045dc7ab30c
Nick <ntoukas.nick@gmail.com>
2022-12-24 14:15:51 -0500

    setup addToCart module - add product to cart array when icon clicked

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -1,6 +1,8 @@
 import { formatPrice, getElement } from '../utils.js';
 import { openCart } from './toggleCart.js';
 
-const addToCartDOM = () => {};
+const addToCartDOM = (product) => {
+  console.log(product);
+};
 
 export default addToCartDOM;
--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -8,9 +8,27 @@ import {
 import { openCart } from './toggleCart.js';
 import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
-// set items
+
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+let cart = getStorageItem('cart');
 
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    product = { ...product, amount: 1 };
+    cart = [...cart, product];
+    addToCartDOM(product);
+  } else {
+  }
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+
+init();
